3df192cb8529645551a7571c367b6a5ee8944f65
armen <babayan_armenzzz@mail.ru>
2022-08-26 11:26:17 +0300

    задача 34

--- Program.cs
+++ Program.cs
@@ -1,85 +1,25 @@
-﻿Console.WriteLine(
-    "Введите не менее восьми элементов через запятую (допустимые символы цифры, запятая)."
-);
-string inputString = string.Empty;
-int countElements = 0;
-int expectedElements = 8;
+﻿using System;
+using static System.Console;
 
-while (countElements < expectedElements)
-{
-    Console.Write($"> {inputString}");
-    inputString += Console.ReadLine();
-    countElements = CountСharacter(inputString, ',') + 1;
-    if (countElements < expectedElements)
-        Console.WriteLine("Недостаточно элементов. Дополните ввод.");
-}
+Clear();
+int[] array = randomArray(4);
+WriteLine($"[{string.Join(",",array)}] -> {countOfEven(array)}");
 
-int startPosition = 0;
-int endPosition = 0;
-int[] resultArray = new int[expectedElements];
-
-for (int i = 0; i < resultArray.Length; i++)
-{
-    endPosition = SearchСharacter(inputString, ',', startPosition + 1) - 1;
-    if (endPosition < 0)
-        endPosition = inputString.Length - 1;
-
-    string textNumber = SubString(inputString, startPosition, endPosition);
-    resultArray[i] = Convert.ToInt32(textNumber);
-
-    startPosition = endPosition + 2;
-}
-
-Console.Write("Результат: ");
-PrintArray(resultArray, '[', ']', ", ", true);
-
-int SearchСharacter(string sourceString, char searchСharacter, int startPosition)
-{
-    for (int i = startPosition; i < sourceString.Length; i++)
-    {
-        if (sourceString[i] == searchСharacter)
-        {
-            return i;
-        }
+int[] randomArray(int size) {
+    Random random = new Random();
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = random.Next(100,1000);
     }
-
-    return -1;
+    return array;
 }
 
-int CountСharacter(string sourceString, char searchСharacter)
-{
+int countOfEven(int[] array) {
     int count = 0;
-    for (int i = 0; i < sourceString.Length; i++)
-    {
-        if (sourceString[i] == searchСharacter)
-        {
+    for(int i = 0; i < array.Length; i++) {
+        if(array[i] % 2 == 0) {
             count++;
         }
     }
-
     return count;
-}
-
-string SubString(string sourceString, int startPosition, int endPosition)
-{
-    string result = String.Empty;
-
-    for (int i = startPosition; i <= endPosition; i++)
-        result += sourceString[i];
-
-    return result;
-}
-
-void PrintArray(int[] array, char startSymbol, char endSymbol, string separator, bool endLine)
-{
-    Console.Write(startSymbol);
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i]);
-        if (i != array.Length - 1)
-            Console.Write(separator);
-    }
-    Console.Write(endSymbol);
-    if (endLine)
-        Console.WriteLine();
 }
