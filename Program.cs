55405d42b58480c60f7ec9555e7cf607b2acd086
armen <babayan_armenzzz@mail.ru>
2022-08-30 14:24:49 +0300

    задача 50

--- Program.cs
+++ Program.cs
@@ -1,33 +1,57 @@
-﻿double[,] randomArray = GetFillArray(3, 4);
-PrintArray(randomArray);
+﻿Console.Write("Введите № строки: ");
+bool inputFirst = int.TryParse(Console.ReadLine(), out int
+numberRows);
 
-double[,] GetFillArray(int m, int n)
+Console.Write("Введите № столбца: ");
+bool inputSecond = int.TryParse(Console.ReadLine(), out int
+numberColumns);
+
+if (inputFirst && inputSecond)
 {
-    double[,] array = new double[m, n];
+    int[,] randomArray = GetFillArray(3, 4);
+    PrintArray(randomArray);
+    Console.WriteLine();
+    FindElementPosition(randomArray, numberRows, numberColumns);
+}
+else Console.WriteLine($"Некорректный ввод!");
+
+
+int[,] GetFillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
     Random randomElement = new Random();
-    int max = 5;
-    int min = -5;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = Math.Round(randomElement.NextDouble()
-                                        * (max - min) + min, 1);
+            array[i, j] = randomElement.Next(10);
         }
     }
     return array;
 }
 
-
-
-void PrintArray(double[,] array)
+void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]} ");
+            Console.Write(array[i, j]);
         }
         Console.WriteLine();
     }
+}
+
+// Строки и столбцы начинаются с 1, для непосвещенного пользователя,
+// Чтобы использовать индексы (от 0),раскоментируйте строки 53 и 54.
+void FindElementPosition(int[,] array, int positionRows, int positionColumns)
+{
+    if (positionRows < array.GetLength(0) && positionColumns < array.GetLength(1))
+    {
+        Console.WriteLine($"На позиции строка № {positionRows}, столбец № {positionColumns} " +
+                          $"находится элемент со значением: {array[positionRows - 1, positionColumns - 1]}");
+        // Console.WriteLine($"На позиции строка № {positionRows}, столбец № {positionColumns} " +
+        //                   $"находится элемент со значением: {array[positionRows, positionColumns]}");
+    }
+    else Console.WriteLine($"Cтрока № {positionRows}, столбец № {positionColumns} - такой позиции в массиве нет.");
 }
